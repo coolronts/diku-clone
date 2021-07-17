9dbe23b841ebc2e003849447fd2a09148749e6d9
Zahid Ronty <58170502+coolronts@users.noreply.github.com>
2021-07-17 15:33:39 +0200

    Delete .history directory

--- .history/src/main_20210615045510.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./index.css";
-import router from "./router/index";
-
-createApp(App).mount("#app");
--- .history/src/main_20210615165815.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./index.css";
-import router from "./router/index";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
-
-createApp(App).use(router).mount("#app");
