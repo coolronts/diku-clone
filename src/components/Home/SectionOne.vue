348413a99daf871713958e729371d07262cccf4d
Zahid Ronty <58170502+coolronts@users.noreply.github.com>
2022-04-04 03:03:29 +0200

    Update SectionOne.vue

--- src/components/Home/SectionOne.vue
+++ src/components/Home/SectionOne.vue
@@ -11,7 +11,7 @@
           Employees
         </router-link>
       </div>
-      <div><img src="https://i.ibb.co/W58Q85N/section1.png"/></div>
+      <div :class="style.image"><img src="https://i.ibb.co/W58Q85N/section1.png" :class="style.image"/></div>
     </div>
   </div>
 </template>
@@ -30,6 +30,7 @@
     style:{
       main:"h-screen flex flex-col justify-between pt-12 px-24 homeSectionOne",
       info:"flex justify-between items-center h-screen",
+      image: "h-full"
       buttons:"pb-24 ml-1",
       button1:"bg-blue-900 px-6 py-3 inline-flex items-center justify-center text-xl font-semibold ring-2 ring-blue-900 text-white hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500",
       button2:"bg-white mx-3 px-6 py-3 inline-flex items-center justify-center text-xl font-semibold text-indigo-900 ring-2 ring-blue-900 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500",
